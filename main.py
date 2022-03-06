17d5c245c05a2147455fa663e16de78ef71e1406
JustAnotherLimaBeans <99616406+JustAnotherLimaBeans@users.noreply.github.com>
2022-03-06 15:10:18 +0000

    main created with basic callback and connect
    Callbacks for on connect and on message as well as connecting to broker

---
+++ main.py
@@ -0,0 +1,16 @@
+import paho.mqtt.client as mqtt
+
+#Callback for connect
+def on_connect(client, userdata, flags, rc):
+    print("Connected to MQTT broker, code: "+str(rc))
+
+#Callback for message receive
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("20.113.148.221", 1883, 60)
+
