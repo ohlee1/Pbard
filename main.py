f051e726d3afa5760cc96e12a206477e5d011f7f
JustAnotherLimaBeans <99616406+JustAnotherLimaBeans@users.noreply.github.com>
2022-03-06 16:37:53 +0000

    Debugging subscribe and publish
    Won't work when running the file, debugging displays but is disjointed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import paho.mqtt.client as mqtt
+import paho
+import asyncio
 
 #Callback for connect
 def on_connect(client, userdata, flags, rc):
@@ -8,9 +10,32 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
+#Callback for publish
+def on_publish(client, userdata, mid):
+    print("Published")
+
+#Callback for subscribe
+def on_subscribe(client, userdata, mid, granted_qos):
+    print("Subscribed")
+
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
+client.on_subscribe = on_subscribe
+client.on_publish = on_publish
 
-client.connect("20.113.148.221", 1183, 60)
+client.tls_set()
 
+client.username_pw_set(username="JustAnotherLiam", password="Liamiot2021")
+async def main():
+
+    client.connect("cefe654d7ef341e290b04311de927a20.s2.eu.hivemq.cloud", 8883, 60)
+
+    print("ur mum")
+
+    client.subscribe("bruh/bruh", qos=0)
+    client.publish("bruh/bruh", "bruhbruhbruh", 0)
+    
+    client.loop_forever()
+
+asyncio.run(main())
