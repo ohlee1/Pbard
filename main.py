b48ca7b0393b8320296b934f32fa3f902a450d15
JustAnotherLimaBeans <99616406+JustAnotherLimaBeans@users.noreply.github.com>
2022-04-12 18:05:58 +0100

    Uploading test file

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+from time import sleep
 import paho.mqtt.client as mqtt
-
-#Global variable for confirmed connection
-
+import asyncio
 
 #Callback for connect
 def on_connect(client, userdata, flags, rc):
@@ -17,7 +16,7 @@ def on_message(client, userdata, msg):
 def on_publish(client, userdata, mid):
     #TODO
     #Make the published callback better
-    print("Published")
+    print(" ", end='')
 
 #Callback for subscribe
 def on_subscribe(client, userdata, mid, granted_qos):
@@ -47,77 +46,38 @@ client.username_pw_set(username="user", password="Jmnb6014")
 #async def main():
 
 def main():
-
-    #Debugging hivemq cluster for connectivity
-    """
+    client.connect("52.232.13.39", 1883, 30)
+    try:
+        #Debugging hivemq cluster for connectivity
+        """
     client.connect("cefe654d7ef341e290b04311de927a20.s2.eu.hivemq.cloud", 8883, 60)
     """
-    integerFlag = 1
 
-    #General debugging as well as connectivity for the moment
-    client.connect("52.232.13.39", 1883, 30)
-
-    try:
-
+        #General debugging as well as connectivity for the moment
+        
+        #string = input("Please enter stuff here for topic: ")
+        stringLiam = "bruh/ollie"
+        stringOllie = "bruh/liam"
+        #stringMsg = input("Please enter message: ")
+        stringMsg = "testbooba"
+        client.subscribe(stringLiam, qos=2)
+        client.publish(stringOllie, stringMsg, qos=2, retain=False)
+        client.loop_start()
+        #Network loop forever
+        #TODO
+        #Find a way to make it loop for subscribe for a certain length maybe with a counter/while loop?
+        #client.loop_forever()
         while True:
-            userChoice = input("Input 1 for sending a message\nInput 2 for checking for messages\nInput 3 for subscribing to a new topic\nInput 0 to exit program\nOption: ")
-            while integerFlag:
-                try:
-                    userChoice = int(userChoice)
-                    integerFlag = 0
-                except ValueError:
-                    print("Please enter a integer...")
-                    userChoice = input("Bruh pls\n")
-                
-            print(f"DEBUGGING USERCHOICE INIT WHILE: {userChoice}")
-
-            while userChoice > 3 or userChoice < 0:
-                    userChoice = int(input("Please enter 1 for sending a message\n2 for checking messages\n3 for subscribing to a topic\n0 to exit the program\nOption: "))
-                    print(f"DEBUGGING USERCHOICE COMPARE WHILE: {userChoice}")
-
-
-
-
-
-
-
-
-
-            if userChoice == 1:
-                #topic = input("Please enter a topic: ")
-                topic = "/bruh/bruh"
-                stringMsg = input("Please enter message: ")
-                client.publish(topic, stringMsg, qos=2, retain=False)
-            
-            elif userChoice == 3:
-                topic = input("Input a topic: ")
-                client.subscribe(topic, qos=2)
-            
-            elif userChoice == 2:
-                #Network loop forever
-                #TODO
-                #Find a way to make it loop for subscribe for a certain length maybe with a counter/while loop?
-                #LIAM: Implemented a small temporary solution using keyboard interrupt to allow check of messages and backout option, elegent solution required here
-                try:
-                    while True:
-                        print("Checking for messages... Press CTRL+C to back out!")
-                        client.loop_forever()
-                except KeyboardInterrupt:
-                    pass
-
-            elif userChoice == 0:
-                print("Exiting program...")
-                exit(0)
-
-            else:
-                print(f"Unknown selector identified\nuserChoice == {userChoice}")
-                print(f"Unknown selector identified\nuserChoice == {userChoice}")
-                print(f"Unknown selector identified\nuserChoice == {userChoice}")
-            integerFlag = 1
-    except KeyboardInterrupt:
-        print("Exiting...")
-        exit(0)
-
+            #client.loop_forever()
+            #sleep(1000)
+            #client.loop_stop()
+            stringMsg = input("You:")
+            client.publish(stringOllie, stringMsg, qos=2, retain=False)
+        
+        #print(stringMsg)
+    except:
+        print("fuck")
+    finally:
+        client.loop_stop()
 if __name__ == '__main__':
     main()
-#asyncio.run(main())
