0bc9d2c75a904efd334bbf4cf36b758b9b774f17
ollie <ollie>
2022-05-06 13:54:04 +0100

    comments in home window py

--- all windows/home-window.py
+++ all windows/home-window.py
@@ -16,6 +16,7 @@ from new_chat_window import Ui_newChatWindow
 
 class Ui_MainWindow(QMainWindow):
     def setupUi(self, MainWindow):
+        #qt designer code begin
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
         self.count=0
@@ -33,6 +34,7 @@ class Ui_MainWindow(QMainWindow):
         self.horizontalLayout.addWidget(self.label)
         self.verticalLayout = QtWidgets.QVBoxLayout()
         self.verticalLayout.setObjectName("verticalLayout")
+        #added button reaction
         self.newChatButton = QtWidgets.QPushButton(self.widget, clicked=lambda: self.openNewChatWindow(MainWindow))
         self.newChatButton.setObjectName("newChatButton")
         self.verticalLayout.addWidget(self.newChatButton)
@@ -49,6 +51,7 @@ class Ui_MainWindow(QMainWindow):
         self.horizontalLayout.addWidget(self.label_2)
         self.gridLayout.addWidget(self.widget, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
+        
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -59,7 +62,9 @@ class Ui_MainWindow(QMainWindow):
         self.newChatButton.setText(_translate("MainWindow", "New Chat"))
         self.openChatButton.setText(_translate("MainWindow", "Open Existing Chat"))
         self.newKeyButton.setText(_translate("MainWindow", "Generate New Key"))
+    #end qt designer code
 
+    #close event, attempts to close the newchatw object when the main window is closed
     def closeEvent(self, event):
         print("Program home screen closed")
         try:
@@ -67,21 +72,31 @@ class Ui_MainWindow(QMainWindow):
         except:
             pass
 
+    #when new chat window button is pressed, execute this
     def openNewChatWindow(self,MainWindow):
+        #attempt to check if the object has been instantiated
+        #if it doesn't exist, go to except and make it
         try:
+            #check if the object exists and if the counter inside is greater than 0
+            #counter will be set to 1 when instantiated and 0 when the window is closed
             if self.newChatW and self.newChatW.count>0:
+                #if window is open and exists and counter is greater than 0, don't open a new one
                 print("new chat window alreay open")
             else:
+                #if window is closed and exists but counter is 0 or less, open a new one
                 self.newChatW = Ui_newChatWindow()
                 self.newChatW.setupUi(self.newChatW)
                 self.newChatW.show()
         except:
+            #create new object
             self.newChatW = Ui_newChatWindow()
+            #call setup function inside the object
             self.newChatW.setupUi(self.newChatW)
+            #show the object
             self.newChatW.show()
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     #MainWindow = QtWidgets.QMainWindow()
     MainWindow = Ui_MainWindow()
     MainWindow.setupUi(MainWindow)
